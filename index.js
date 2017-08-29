1d3ee481d1fca9360df9b07b5c4cc5cddd3e38c0
Scott Ferguson <esfergus@gmail.com>
2017-08-29 18:23:37 -0500

    Added package.json with configurations. Renames js file to index

---
+++ index.js
@@ -0,0 +1,313 @@
+var Alexa = require('alexa-sdk');
+var http = require('http');
+
+var states = {
+    SEARCHMODE: '_SEARCHMODE',
+    TOPFIVE: '_TOPFIVE',
+};
+
+var location = "Little Rock";
+
+var numberOfResults = 3;
+
+var APIKey = process.env.API_KEY;
+
+var welcomeMessage = location + " Guide. You can ask me for an attraction, the local news, or  say help. What will it be?";
+
+var welcomeRepromt = "You can ask me for an attraction, the local news, or  say help. What will it be?";
+
+var locationOverview = "Little Rock is the capital city of the state of Arkansas. With an estimated 193,524 residents as of 2010, Little Rock is the largest city in the state of Arkansas. ";
+
+var HelpMessage = "Here are some things you  can say: Give me an attraction. Tell me about " + location + ". Tell me the top five things to do. Tell me the local news.  What would you like to do?";
+
+var moreInformation = "See your  Alexa app for  more  information.";
+
+var tryAgainMessage = "please try again.";
+
+var noAttractionErrorMessage = "What attraction was that? " + tryAgainMessage;
+
+var topFiveMoreInfo = " You can tell me a number for more information. For example, open number one.";
+
+var getMoreInfoRepromtMessage = "What number attraction would you like to hear about?";
+
+var getMoreInfoMessage = "OK, " + getMoreInfoRepromtMessage;
+
+var goodbyeMessage = "OK, have a nice time in " + location + ".";
+
+var newsIntroMessage = "These are the " + numberOfResults + " most recent " + location + " headlines, you can read more on your Alexa app. ";
+
+var hearMoreMessage = "Would you like to hear about another top thing that you can do in " + location +"?";
+
+var newline = "\n";
+
+var output = "";
+
+var alexa;
+
+var attractions = [
+    { name: "Little Rock Central High School", content: "Interpreting the Civil Rights Movement with an emphasis on school integration, this National Historic Site focuses on the 1957 desegregation crisis that resulted when Arkansas Governor Orval Faubus ordered National Guard troops to prevent the first African American students The Little Rock Nine from entering formerly all white Little Rock Central High School, leading to federal intervention by order of President Dwight Eisenhower." },
+    { name: "Pinnacle Mountain State Park", content: "Opened in 1977, the Pinnacle Mountain State Park serves as a center for education, recreation and preservation." },
+    { name: "William J. Clinton Presidential Library", content: "The William J. Clinton Presidential Center and Park, located on the banks of the Arkansas River in Little Rock, Arkansas, attracts hundreds of thousands of visitors from around the world to its grounds each year. Opened in 2004, the Center is home to the Little Rock offices of the Clinton Foundation, the William J. Clinton Presidential Library and Museum, and the University of Arkansas Clinton School of Public Service." },
+    { name: "Big Dam Bridge", content: "Experience the longest pedestrian and bicycle bridge in North America, built specifically for that use. This impressive structure was named the Big Dam Bridge because of its massive 4,226 foot span built atop Murray Lock and Dam. Elevated up to 90 feet above the Arkansas River, the Big Dam Bridge connects over 14 miles of scenic riverside trails in the cities of Little Rock and North Little Rock, and assists in the connection of 70,000 acres of various city, county, state and federal park land." },
+    { name: "Riverfront Park", content: "Riverfront Park stretches eleven blocks on the south bank of the Arkansas River in downtown Little Rock. The Park provides 33 acres of urban parkland for outdoor events, leisure activities and a glimpse of the state's history. " },
+];
+
+var topFive = [
+    { number: "1", caption: "Visit the William J. Clinton Presidential Library.", more: "The Clinton Library holds the collections of Bill Clinton's presidency. It also has a wonderful restaurant, 42, and a replica of the Oval Office. Follow your visit with a stroll along the pedestrian bridge and park." },
+    { number: "2", caption: "Get lunch at the River Market.", more: "Resting on one of the banks of the Arkansas River, the River Market District was developed in the 1990s to become one of the most vibrant areas of the city with a range of different attractions on show.One of these is the Ottenheimer Market Hall which is a food area where you will find old fashioned stalls run by a variety of vendors.There is also a farmers’ market here as well as the River Market Tower.", location: "Pike Place Market PDA, 85 Pike Street, Room 500, Seattle, WA 98101", contact: "info@pikeplacemarket.org \n 206 682 7453" },
+    { number: "3", caption: "Walk along the Big Dam Bridge.", more: "Big Dam Bridge has the claim to fame of being the longest pedestrian and cycle bridge in the whole of the United States.The bridge links over 20 miles of trails that skirt along the river and acts as a connection between Little Rock and North Little Rock." },
+    { number: "4", caption: "Discover Arkansas wildlife at the William E.Clark Presidential Park Wetlands.", more: "The Bill Clark Presidential Park Wetlands involves the restoration of 13 acres of Arkansas wetlands along the Arkansas River.The Wetlands is designed to showcase wildlife and river life in a restored wetlands habitat for the education and enjoyment of millions of national and international visitors to the Clinton Presidential Park and Library." },
+    { number: "5", caption: "Visit the Old State House.", more: "The Old State House is known in Little Rock for its cultural and historical significance and was also the place where Bill Clinton celebrated his election night during the presidential race in 1992. The building dates from 1833 and is now a United States National Historic Landmark, and with that in mind it has also been turned into a museum." },
+];
+
+var topFiveIntro = "Here are the top five things to  do in " + location + ".";
+
+var newSessionHandlers = {
+    'LaunchRequest': function () {
+        this.handler.state = states.SEARCHMODE;
+        output = welcomeMessage;
+        this.emit(':ask', output, welcomeRepromt);
+    },
+    'getOverview': function () {
+        this.handler.state = states.SEARCHMODE;
+        this.emitWithState('getOverview');
+    },
+    'getNewsIntent': function () {
+        this.handler.state = states.SEARCHMODE;
+        this.emitWithState('getNewsIntent');
+    },
+    'getAttractionIntent': function () {
+        this.handler.state = states.SEARCHMODE;
+        this.emitWithState('getAttractionIntent');
+    },
+    'getTopFiveIntent': function(){
+        this.handler.state = states.SEARCHMODE;
+        this.emitWithState('getTopFiveIntent');
+    },
+    'AMAZON.StopIntent': function () {
+        this.emit(':tell', goodbyeMessage);
+    },
+    'AMAZON.CancelIntent': function () {
+        // Use this function to clear up and save any data needed between sessions
+        this.emit(":tell", goodbyeMessage);
+    },
+    'SessionEndedRequest': function () {
+        // Use this function to clear up and save any data needed between sessions
+        this.emit('AMAZON.StopIntent');
+    },
+    'Unhandled': function () {
+        output = HelpMessage;
+        this.emit(':ask', output, welcomeRepromt);
+    },
+};
+
+var startSearchHandlers = Alexa.CreateStateHandler(states.SEARCHMODE, {
+    'getOverview': function () {
+        output = locationOverview;
+        this.emit(':tellWithCard', output, location, locationOverview);
+    },
+    'getAttractionIntent': function () {
+        var cardTitle = location;
+        var cardContent = "";
+
+        var attraction = attractions[Math.floor(Math.random() * attractions.length)];
+        if (attraction) {
+            output = attraction.name + " " + attraction.content + newline + moreInformation;
+            cardTitle = attraction.name;
+            cardContent = attraction.content + newline + attraction.contact;
+
+            this.emit(':tellWithCard', output, cardTitle, cardContent);
+        } else {
+            this.emit(':ask', noAttractionErrorMessage, tryAgainMessage);
+        }
+    },
+    'getTopFiveIntent': function () {
+        output = topFiveIntro;
+        var cardTitle = "Top Five Things To See in " + location;
+
+        for (var counter = topFive.length - 1; counter >= 0; counter--) {
+            output += " Number " + topFive[counter].number + ": " + topFive[counter].caption + newline;
+        }
+        output += topFiveMoreInfo;
+        this.handler.state = states.TOPFIVE;
+        this.emit(':askWithCard', output, topFiveMoreInfo, cardTitle, output);
+    },
+    'AMAZON.YesIntent': function () {
+        output = HelpMessage;
+        this.emit(':ask', output, HelpMessage);
+    },
+    'AMAZON.NoIntent': function () {
+        output = HelpMessage;
+        this.emit(':ask', HelpMessage, HelpMessage);
+    },
+    'AMAZON.StopIntent': function () {
+        this.emit(':tell', goodbyeMessage);
+    },
+    'AMAZON.HelpIntent': function () {
+        output = HelpMessage;
+        this.emit(':ask', output, HelpMessage);
+    },
+    'getNewsIntent': function () {
+        httpGet(location, function (response) {
+
+            // Parse the response into a JSON object ready to be formatted.
+            var responseData = JSON.parse(response);
+            var cardContent = "Data provided by New York Times\n\n";
+
+            // Check if we have correct data, If not create an error speech out to try again.
+            if (responseData == null) {
+                output = "There was a problem with getting data please try again";
+            }
+            else {
+                output = newsIntroMessage;
+
+                // If we have data.
+                for (var i = 0; i < responseData.response.docs.length; i++) {
+
+                    if (i < numberOfResults) {
+                        // Get the name and description JSON structure.
+                        var headline = responseData.response.docs[i].headline.main;
+                        var index = i + 1;
+
+                        output += " Headline " + index + ": " + headline + ";";
+
+                        cardContent += " Headline " + index + ".\n";
+                        cardContent += headline + ".\n\n";
+                    }
+                }
+
+                output += " See your Alexa app for more information.";
+            }
+
+            var cardTitle = location + " News";
+
+            alexa.emit(':tellWithCard', output, cardTitle, cardContent);
+        });
+    },
+
+    'AMAZON.RepeatIntent': function () {
+        this.emit(':ask', output, HelpMessage);
+    },
+    'AMAZON.CancelIntent': function () {
+        // Use this function to clear up and save any data needed between sessions
+        this.emit(":tell", goodbyeMessage);
+    },
+    'SessionEndedRequest': function () {
+        // Use this function to clear up and save any data needed between sessions
+        this.emit('AMAZON.StopIntent');
+    },
+    'Unhandled': function () {
+        output = HelpMessage;
+        this.emit(':ask', output, welcomeRepromt);
+    }
+});
+
+var topFiveHandlers = Alexa.CreateStateHandler(states.TOPFIVE, {
+    'getAttractionIntent': function () {
+        this.handler.state = states.SEARCHMODE;
+        this.emitWithState('getAttractionIntent');
+    },
+    'getOverview': function () {
+        this.handler.state = states.SEARCHMODE;
+        this.emitWithState('getOverview');
+    },
+    'getTopFiveIntent': function () {
+        this.handler.state = states.SEARCHMODE;
+        this.emitWithState('getTopFiveIntent');
+    },
+    'AMAZON.HelpIntent': function () {
+        output = HelpMessage;
+        this.emit(':ask', output, HelpMessage);
+    },
+
+    'getMoreInfoIntent': function () {
+        var slotValue = 0;
+        if(this.event.request.intent.slots.attraction ) {
+            if (this.event.request.intent.slots.attraction.value) {
+                slotValue = this.event.request.intent.slots.attraction.value;
+            }
+        }
+
+        if (slotValue > 0 && slotValue <= topFive.length) {
+            var index = parseInt(slotValue) - 1;
+            var selectedAttraction = topFive[index];
+
+            output = selectedAttraction.caption + ". " + selectedAttraction.more + ". " + hearMoreMessage;
+            var cardTitle = selectedAttraction.name;
+            var cardContent = selectedAttraction.caption + newline + newline + selectedAttraction.more + newline + newline + selectedAttraction.location + newline + newline + selectedAttraction.contact;
+
+            this.emit(':askWithCard', output, hearMoreMessage, cardTitle, cardContent);
+        } else {
+            this.emit(':ask', noAttractionErrorMessage);
+        }
+    },
+
+    'AMAZON.YesIntent': function () {
+        output = getMoreInfoMessage;
+        alexa.emit(':ask', output, getMoreInfoRepromtMessage);
+    },
+    'AMAZON.NoIntent': function () {
+        output = goodbyeMessage;
+        alexa.emit(':tell', output);
+    },
+    'AMAZON.StopIntent': function () {
+        this.emit(':tell', goodbyeMessage);
+    },
+    'AMAZON.RepeatIntent': function () {
+        this.emit(':ask', output, HelpMessage);
+    },
+    'AMAZON.CancelIntent': function () {
+        // Use this function to clear up and save any data needed between sessions
+        this.emit(":tell", goodbyeMessage);
+    },
+    'SessionEndedRequest': function () {
+        // Use this function to clear up and save any data needed between sessions
+    },
+
+    'Unhandled': function () {
+        output = HelpMessage;
+        this.emit(':ask', output, welcomeRepromt);
+    }
+});
+
+exports.handler = function (event, context, callback) {
+    alexa = Alexa.handler(event, context);
+    alexa.registerHandlers(newSessionHandlers, startSearchHandlers, topFiveHandlers);
+    alexa.execute();
+};
+
+// Create a web request and handle the response.
+function httpGet(query, callback) {
+    console.log("/n QUERY: "+query);
+
+    var options = {
+        //http://api.nytimes.com/svc/search/v2/articlesearch.json?q=seattle&sort=newest&api-key=
+        host: 'api.nytimes.com',
+        path: '/svc/search/v2/articlesearch.json?q=' + query + '&sort=newest&api-key=' + APIKey,
+        method: 'GET'
+    };
+
+    var req = http.request(options, (res) => {
+
+            var body = '';
+
+    res.on('data', (d) => {
+        body += d;
+});
+
+    res.on('end', function () {
+        callback(body);
+    });
+
+});
+    req.end();
+
+    req.on('error', (e) => {
+        console.error(e);
+});
+}
+
+String.prototype.trunc =
+    function (n) {
+        return this.substr(0, n - 1) + (this.length > n ? '&hellip;' : '');
+    };
